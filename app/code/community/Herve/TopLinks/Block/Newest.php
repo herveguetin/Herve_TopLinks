da37ca7dcda53950dca3cf900054162b165e4d93
Hervé Guétin <herve.guetin@gmail.com>
2013-08-30 03:08:34 +0200

    Initial commit

---
+++ app/code/community/Herve/TopLinks/Block/Newest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @category    Herve
+ * @package     Herve_TopLinks
+ * @copyright   Copyright (c) 2013 Hervé Guétin (http://www.herveguetin.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Herve_TopLinks_Block_Newest extends Mage_Core_Block_Template {
+
+    public function addBuyNewestLink()
+    {
+        $parentBlock = $this->getParentBlock();
+
+        $collection = Mage::getResourceModel('catalog/product_collection');
+        Mage::getModel('catalog/layer')->prepareProductCollection($collection);
+        $collection->setOrder('created_at')
+            ->addStoreFilter()
+            ->setPageSize(1);
+
+        $newestProduct = $collection->getFirstItem();
+        $label = $this->__("Add '%s' to cart now!", $newestProduct->getName());
+
+        $parentBlock->addLink($label, 'checkout/cart/add', $label, true, array('product' => $newestProduct->getId()), 1000);
+        return $this;
+    }
+}
---
+++ app/code/community/Herve/TopLinks/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @category    Herve
+ * @package     Herve_TopLinks
+ * @copyright   Copyright (c) 2013 Hervé Guétin (http://www.herveguetin.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Herve_TopLinks_Helper_Data extends Mage_Core_Helper_Abstract {}
